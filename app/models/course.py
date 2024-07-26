608e2be99e95c5d18e1f826e6bcb7a651991208b
Franco <franmarinmarin10@gmail.com>
2024-07-26 01:55:56 -0300

    agrego el modelo course

---
+++ app/models/course.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Course(db.Model):
+    __tablename__ = "courses"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), nullable = False)
