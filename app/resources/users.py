b9454e73324178e179934ccbcf5d106f53e04964
Franco <franmarinmarin10@gmail.com>
2024-07-27 16:40:37 -0300

    agrego endpoint para obtener todos los users

--- app/resources/users.py
+++ app/resources/users.py
@@ -13,14 +13,14 @@ def create_user():
     email = data.get("email")
     password = data.get("password")
     role_name = data.get("role")
-
+    
     #Validations
     if not email or not password:
         return jsonify({"message": "Email and password are required"}), 400
-
+    
     if User.query.filter_by(email = email).first():
         return jsonify({"message": "Email already registered"}), 400
-
+    
     role = Role.query.filter_by(name = role_name).first()
     if not role:
         return jsonify({"message": "Role not found"}), 400
@@ -35,16 +35,30 @@ def create_user():
     )
     db.session.add(user_data)
     db.session.commit()
-
+    
     #User creation
     user = User(email = email, userdata = user_data)
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
-
+    
     #Profile creation for User
     profile = Profile(user_id = user.id, role_id = role.id)
     db.session.add(profile)
     db.session.commit()
 
     return jsonify({"message": "User registered successfully", "id": user.id}), 201
+
+@user_bp.route("/users", methods = ["GET"])
+def get_all_users():
+    users = User.query.all()
+    users_list = []
+    for user in users:
+        data = {
+            "id" : user.id,
+            "email" : user.email,
+            "first_name" : user.userdata.first_name,
+            "last_name" : user.userdata.last_name
+        }
+        users_list.append(data)
+    return jsonify({"users": users_list}), 200
