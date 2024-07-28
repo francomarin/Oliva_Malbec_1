5e50d3256fa21c55c9d596575f7809d7d7e36758
Franco <franmarinmarin10@gmail.com>
2024-07-28 03:23:03 -0300

    agrego test de "get_all_courses"

--- tests/tests_bp_courses.py
+++ tests/tests_bp_courses.py
@@ -40,6 +40,24 @@ class TestCourseBP(unittest.TestCase):
         self.assertEqual(created_course.name, "test")
         self.assertEqual(created_course.id, created_course_id)
 
+    def test_get_all_courses(self):
+        course1 = Course(name = "course1")
+        course2 = Course(name = "course2")
+        db.session.add(course1)
+        db.session.add(course2)
+        db.session.commit()
+
+        response = self.client.get("/courses")
+
+        self.assertEqual(response.status_code, 200)
+        courses = response.json["courses"]
+
+        self.assertIsNotNone(courses)
+
+        courses_names = [course["name"] for course in courses]
+        self.assertIn("course1", courses_names)
+        self.assertIn("course2", courses_names)
+
     def test_get_course_by_id(self):
         course = Course(name = "test")
         db.session.add(course)
