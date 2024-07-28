526f833094b2d2200cca1ac41576dc62538a8662
Franco <franmarinmarin10@gmail.com>
2024-07-28 03:11:30 -0300

    agrego test de "get_course_by_id"

--- tests/tests_bp_courses.py
+++ tests/tests_bp_courses.py
@@ -40,6 +40,17 @@ class TestCourseBP(unittest.TestCase):
         self.assertEqual(created_course.name, "test")
         self.assertEqual(created_course.id, created_course_id)
 
+    def test_get_course_by_id(self):
+        course = Course(name = "test")
+        db.session.add(course)
+        db.session.commit()
+
+        course_id = course.id
+        response = self.client.get(f"/courses/{course_id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json["course"]["id"], course_id)
+        self.assertEqual(response.json["course"]["name"], "test")    
+
     def test_update_course(self):
         course = Course(name = "test")
         db.session.add(course)
